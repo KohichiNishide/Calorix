3f49c74c40cef7fee8c32efe4bfc1a9d2bd6162c
kNishide <kohichi.nishide@nts.ricoh.co.jp>
2014-09-13 10:14:41 +0900

    tableViewを追加

---
+++ Calorix/MainViewController.swift
@@ -0,0 +1,40 @@
+//
+//  MainViewController.swift
+//  Calorix
+//
+//  Created by KoichiNishide on 2014/09/13.
+//  Copyright (c) 2014年 KoichiNishide. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var texts = ["hello", "world", "hello", "Swift"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.texts.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
+        cell.textLabel?.text = "Row #\(indexPath.row)"
+        cell.detailTextLabel?.text = "Subtitle #\(indexPath.row)"
+        return cell
+    }
+    
+    func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
+        println("You selected cell #\(indexPath.row)!")
+    }
+}
