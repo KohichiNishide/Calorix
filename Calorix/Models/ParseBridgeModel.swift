20edb65e893040b59862a8a82c1fbe1d6791e4a5
kNishide <kohichi.nishide@nts.ricoh.co.jp>
2014-09-17 21:55:03 +0900

    Foodに戻す

--- Calorix/Models/ParseBridgeModel.swift
+++ Calorix/Models/ParseBridgeModel.swift
@@ -15,34 +15,20 @@ class ParseBridgeModel {
     let calorieKey = "calorie"
     
     func asyncLoadAllMeals () {
-        var query: PFQuery = PFQuery(className: mealClassName)
-        query.findObjectsInBackgroundWithBlock({(meals: [AnyObject]!, error: NSError!) in
+        var query: PFQuery = PFQuery(className: foodClassName)
+        query.findObjectsInBackgroundWithBlock({(foods: [AnyObject]!, error: NSError!) in
             if (error != nil) {
                 NSLog("error " + error.localizedDescription)
                 return
             }
-            for meal in meals {
-                self.asyncLoadFoods (meal as PFObject)
-            }
-        })
-    }
-    
-    func asyncLoadFoods (meal: PFObject) {
-        var relation: PFRelation = meal.relationForKey(self.foodsKey)
-        var foodsQuery: PFQuery = relation.query()
-        foodsQuery.findObjectsInBackgroundWithBlock({(foods: [AnyObject]!, error: NSError!) in
-            if (error != nil) {
-                NSLog("error " + error.localizedDescription)
-                return
-            }
-            var reloadedFoods = [Food]()
+            var loadedFoods:[Food] = []
             for food in foods {
                 var name = food.objectForKey(self.nameKey) as String?
                 var calorie = food.objectForKey(self.calorieKey) as Int?
-                var reloadedFood = Food(name: name!, calorie: calorie!)
-                reloadedFoods.append(reloadedFood)
+                var loadedFood = Food(name: name!, calorie: calorie!)
+                loadedFoods.append(loadedFood)
             }
-            self.delegate?.loadedAllFood(reloadedFoods)
+            self.delegate?.loadedAllFood(loadedFoods)
         })
     }
 }
